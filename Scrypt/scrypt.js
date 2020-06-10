aedf4d2cd6ddf9c966196d220545739a0a71c6ba
m3l1k4 <melikaihelas@gmail.com>
2020-06-10 10:17:56 -0700

    .

---
+++ Scrypt/scrypt.js
@@ -0,0 +1,12 @@
+let apiKey = "$2a$10$fFLgx9jmgVkDfvD3XPaHo.z9shUBKZuhJWsCyOleSak2VfH1zMmve";
+
+testFunction=() => {
+    axios.get("https://www.potterapi.com/v1/sortingHat")
+    .then(response => {
+        let house=response.data
+        document.querySelector(".house__title").innerHTML = house
+    })
+};
+
+testFunction();
+
