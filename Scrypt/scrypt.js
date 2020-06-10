e22ec63b9bc9b2e7dc864186ffc958abb3c0a246
m3l1k4 <melikaihelas@gmail.com>
2020-06-10 11:00:53 -0700

    Update scrypt.js

--- Scrypt/scrypt.js
+++ Scrypt/scrypt.js
@@ -1,37 +1,44 @@
 let apiKey = "$2a$10$fFLgx9jmgVkDfvD3XPaHo.z9shUBKZuhJWsCyOleSak2VfH1zMmve";
 
-testFunction=() => {
+getApi=() => {
     axios.get("https://www.potterapi.com/v1/sortingHat")
     .then(response => {
-        let house=response.data
-        document.querySelector(".house__title").innerHTML = house
-    });
-    
+        let house=response.data;
+        document.querySelector(".house__title").innerHTML = house;
    
+    })
+    
+  
 };
 
-testFunction();
+getApi();
 
+//myHouse();
 
-
-function myHouse(houseName){
+/*
+function myHouse(){
+   // houseName=
 
     if ( houseName=="Gryffindor"){
         document.querySelector(".house_sigil").style.backgroundImage= "url('./assets/Gryffindor.png')";
     }
 
     else if (houseName=="Hufflepuff"){
+        document.querySelector(".house_sigil").style.backgroundImage= "url('./assets/Hufflepuff.png')";
 
     }
 
     else if (houseName=="Slytherin"){
+        document.querySelector(".house_sigil").style.backgroundImage= "url('./assets/Slytherin.png')";
 
     }
 
     else if (houseName=="Ravenclaw"){
+        document.querySelector(".house_sigil").style.backgroundImage= "url('./assets/Ravenclaw.png')";
 
     }
 
    
 
 };
+*/
